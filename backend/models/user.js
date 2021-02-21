a25b475ded2494d87ad47d7bc11a5dd747eb6183
Mantzy <fanni.kovacs@hotmail.com>
2021-02-21 21:05:59 +0000

    axios update

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,8 +3,10 @@ const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
     //userId: { type: String, required: true, unique: true },
+    name: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    profile: { type: String, required: false }
 });
 
 userSchema.plugin(uniqueValidator);
--- vue-frontend/src/views/Register.vue
+++ vue-frontend/src/views/Register.vue
@@ -6,15 +6,15 @@
                                 <div class="form-group">
                                     <div class="form-group">
                                         <label for="Name1">Name</label>
-                                        <input type="text" class="form-control" id="Name1" placeholder="Name">
+                                        <input type="text" class="form-control" id="Name1" placeholder="Name" v-model="form.name">
                                     </div>
                                     <label for="Email1">Email address</label>
-                                    <input type="email" class="form-control" id="Email1" aria-describedby="emailHelp" placeholder="Enter email">
+                                    <input type="email" class="form-control" id="Email1" aria-describedby="emailHelp" placeholder="Enter email" v-model="form.email">
                                     <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                                 </div>
                                 <div class="form-group">
                                     <label for="Password1">Password</label>
-                                    <input type="password" class="form-control" id="Password1" placeholder="Password">
+                                    <input type="password" class="form-control" id="Password1" placeholder="Password" v-model="form.password">
                                 </div>
                                 <div class="form-group">
                                     <label for="profilePicture">Choose a profile picture</label>
@@ -22,7 +22,7 @@
                                 </div>
 
                                 <div class="text-right">
-                                    <button type="submit" class="btn btn-color">Register</button>
+                                    <button type="submit" class="btn btn-color" @click="register()">Register</button>
                                 </div>
                                                                                                 <div class="text-right">
                                     <p>Already a member? <router-link to="/login">Log in here</router-link></p>
@@ -33,7 +33,35 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
+name: "register",
+
+data() {
+    return {
+      form: {
+        name: "",
+        email: "",
+        password: "",
+        profile: "",
+      },
+    };
+  },
+
+  methods: {
+       register() {
+           axios.post("http://localhost:3000/api/auth/signup", this.form).then((response) => {
+        window.location.href="#/login"
+                 console.log(response)
+
+
+
+})
+
+      }
+  }
+
 
 }
 </script>
